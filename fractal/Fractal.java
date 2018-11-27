7d64b7e7c11cc705e0c5e197f493de3764adf1c8
SomeDudeOnThisPage <robin.buhlmann@googlemail.com>
2018-11-27 18:20:47 +0100

    Add files via upload

--- fractal/Fractal.java
+++ fractal/Fractal.java
@@ -11,8 +11,38 @@ public class Fractal extends JComponent {
 	private static final long serialVersionUID = 1L;
 	private int scale;
 	private int currentStep;
+	private int oldx;
+	private int oldy;
 	private int x;
 	private int y;
+	
+	private void moveHilbert(int j, int h, Graphics g) {
+		oldy = y;
+		oldx = x;
+		if (j == 1) {
+			y = y - h;
+		} else if (j == 2) {
+			x = x + h;
+		} else if (j == 3) {
+			y = y + h;
+		} else if (j == 4) {
+			x = x - h;
+		}
+		g.drawLine(oldx, oldy, x, y);
+	}
+	
+	private void hilbert(int r, int d, int l, int u, int i, int h, Graphics g) {
+		if (i > 0) {
+			i--;
+			hilbert(d,r,u,l,i,h,g);
+			moveHilbert(r,h,g);
+			hilbert(r,d,l,u,i,h,g);
+			moveHilbert(d,h,g);
+			hilbert(r,d,l,u,i,h,g);
+			moveHilbert(l,h,g);
+			hilbert(u,l,d,r,i,h,g); 
+		}
+	}
 	
 	@Override
    protected void paintComponent(Graphics g) {
@@ -21,23 +51,19 @@ public class Fractal extends JComponent {
       Graphics2D graph2 = (Graphics2D) g;
         
       graph2.setColor(Color.BLACK);
+      oldx = 0;
+      oldy = 0;
+      this.y = 0;
+      this.x = 0;
+      int h = 10;
+      int r=2;
+      int d=3;
+      int l=4;
+      int u=1; 
       
-      // DACH
-      graph2.drawLine(scale, 0, 0, scale);
-      graph2.drawLine(scale, 0, 2*scale, scale);
-      graph2.drawLine(0, scale, 2*scale, scale);
-        
-      // HAUS
-      graph2.drawLine(0, scale, 0, 3*scale);
-      graph2.drawLine(2*scale, scale, 2*scale, 3*scale);
-      graph2.drawLine(0, 3*scale, 2*scale, 3*scale);
-        
-      // MITTELBALKENDINGER
-      graph2.drawLine(0, scale, 2*scale, 3*scale);
-      graph2.drawLine(2*scale, scale, 0, 3*scale);
-        
-      graph2.drawString("Ich drawe more lines als ein kokainjunkie am bahnhof", 10, 350);
-      graph2.drawString("Pass auf oder ich komm und klopp dich zusammen, dann machste nie mehr ferien auf dem reiterhof", 10, 375);
+      int n = 5;
+      
+      hilbert(r,d,l,u,n,h,g); 
 		}
 	
 	public Fractal(int scale) {
