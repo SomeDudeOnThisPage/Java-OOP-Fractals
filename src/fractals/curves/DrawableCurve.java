26ff52fc4d844c6f3c7d65c229ea80d733b2650f
SomeDudeOnThisPage <robin.buhlmann@googlemail.com>
2018-12-09 16:18:39 +0100

    Delete DrawableCurve.java

--- src/fractals/curves/DrawableCurve.java
+++
@@ -1,7 +0,0 @@
-package fractals.curves;
-
-import java.awt.*;
-
-public abstract class DrawableCurve {
-    public abstract Graphics generate(Graphics g, float scaleFactor, int startX, int startY, int iterations);
-}
