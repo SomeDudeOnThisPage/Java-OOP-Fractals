d5a748d983b926f0d6a5ccb8d8ba2fbdfe9d6d55
Robin <robin.buhlmann@gmail.com>
2018-12-12 19:36:18 +0100

    hehe I actually forgot to include the curve class w h o o p s

---
+++ src/program/system/Curve.java
@@ -0,0 +1,40 @@
+package program.system;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Abstract class serving as a template for various algorithms
+ * <p>
+ *   This class contains the shared variables between all algorithms
+ * </p>
+ * @author Robin Buhlmann
+ * @version 0.1
+ */
+public abstract class Curve {
+  /**
+   * The size of the canvas that holds this curve.
+   * The canvas is squared so no x/y are needed.
+   */
+  private int csize;
+
+  /**
+   * The scale for the algorithm.
+   */
+  private int scale;
+
+  /**
+   *
+   * @return BufferedImage
+   */
+  public abstract BufferedImage render();
+
+  /**
+   * Constructor for the curve class
+   * @param csize The size of the canvas
+   * @param scale The scale of the curve
+   */
+  public Curve(int csize, int scale) {
+    this.csize = csize;
+    this.scale = scale;
+  }
+}
